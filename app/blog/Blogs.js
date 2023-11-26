7fefa849c18412cf7db5f4213c7667e3f8034144
tito-arch <therabbitplc@gmail.com>
2023-11-27 01:42:30 +0300

    Feat: Fetch and Display Random Tech Blog Previews
    - Refactored the blog fetching logic to use `fetch` instead of `axios`.
    - Implemented parsing of XML data using `DOMParser`.
    - Updated the component to display a random tech-themed preview for each article.
    - Added pagination for better user experience.
    - Improved error handling and added a link to the main site if data fetching fails.

--- app/blog/Blogs.js
+++ app/blog/Blogs.js
@@ -1,56 +1,137 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { parse } from "fast-xml-parser";
+import React, { useState, useEffect } from "react";
+import he from "he";
 
-function Blogs() {
-  const [xmlData, setXmlData] = useState([]);
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const generateRandomText = (text) => {
+  return text.split("").map((letter, index) => (
+    <span key={index} style={{ color: getRandomColor() }}>
+      {letter}{" "}
+    </span>
+  ));
+};
+
+const MyArticles = () => {
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(15);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("https:bunnieabc.com/index.xml")
-      .then((response) => {
-        const xmlText = response.data;
-        console.log("Raw XML Data:", xmlText); // Log the raw XML data
+    async function fetchData() {
+      try {
+        const res = await fetch("https://www.bunnieabc.com/index.xml");
+        const xmlText = await res.text();
 
-        const jsonData = parse(xmlText, {
-          ignoreAttributes: false,
-          attributeNamePrefix: "_",
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+
+        const itemNodes = xmlDoc.querySelectorAll("item");
+
+        const items = Array.from(itemNodes).map((itemNode) => {
+          const title = he.decode(itemNode.querySelector("title").textContent);
+          const preview = title
+            .split(" ")
+            .slice(0, 3)
+            .map((word) => word.charAt(0))
+            .join("");
+
+          return {
+            title,
+            link: itemNode.querySelector("link").textContent,
+            pubDate: itemNode.querySelector("pubDate").textContent,
+            description: he.decode(
+              itemNode.querySelector("description").textContent
+            ),
+            preview,
+          };
         });
 
-        // Check if the XML structure matches your expectations
-        const items = jsonData.rss?.channel?.item;
-
-        if (Array.isArray(items)) {
-          setXmlData(items);
-        } else if (items) {
-          // If there's only one item, wrap it in an array
-          setXmlData([items]);
-        } else {
-          console.error("No 'item' elements found in XML data.");
-        }
-      })
-      .catch((error) => {
+        setItems(items);
+      } catch (error) {
+        setError(true);
         console.error("Error fetching or parsing XML data:", error);
-      });
+      }
+    }
+
+    fetchData();
   }, []);
-  console.log(xmlData);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = items.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  if (error) {
+    return (
+      <section>
+        <div>
+          <h3>Latest Articles</h3>
+          <ul>
+            <p>Failed to fetch data, please try again later.</p>
+          </ul>
+          <a
+            href={"https://bunnieabc.com/"}
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            Visit Bunnie ABC
+          </a>
+        </div>
+      </section>
+    );
+  }
 
   return (
-    <div>
-      <h2>Blog Posts</h2>
-      <ul>
-        {xmlData.map((post, index) => (
-          <li key={index}>
-            <a href={post.link._text}>{post.title._text}</a>
-            <p>{post.description._text}</p>
-            <p>Published on: {post.pubDate._text}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section>
+      <div>
+        <h3>Tech Articles</h3>
+        <ul className="flex flex-col gap-2">
+          {currentPosts.map((item, index) => (
+            <div key={index} className="flex border-black">
+              <div
+                className="flex w-[200px]"
+                style={{ backgroundColor: getRandomColor(), padding: "20px" }}
+              >
+                {generateRandomText(item.preview)}
+              </div>
+              <div className="flex flex-col">
+                <a href={item.link} target={"_blank"}>
+                  <h3>{item.title}</h3>
+                </a>
+                <p>{item.description}</p>
+                <p>{item.pubDate}</p>
+              </div>
+            </div>
+          ))}
+        </ul>
+        <div>
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={indexOfLastPost >= items.length}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
-export default Blogs;
+export default MyArticles;
