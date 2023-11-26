c5d590e8a2574e65caf2e340038cbdd5a973b4d2
tito-arch <therabbitplc@gmail.com>
2023-11-26 15:13:55 +0300

    Add: local proxy server
    This commit adds a new local proxy server to escape cors errors on local development

---
+++ src/LocalProxy.js
@@ -0,0 +1,20 @@
+// Install the 'express' package if you haven't already
+// npm install express
+
+// Create a file named server.js
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use(
+  "/xml",
+  createProxyMiddleware({
+    target: "https://www.bunnieabc.com",
+    changeOrigin: true,
+  })
+);
+
+app.listen(3001, () => {
+  console.log("Proxy server is running on http://localhost:3001");
+});
