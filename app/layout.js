6b96ad3be184261d183225bb5e1cf2e3be583e68
tito-arch <therabbitplc@gmail.com>
2024-01-28 03:34:48 +0300

    Fix: render nav and footer in all pages by default

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import Footer from "@/components/footer/Footer";
+import Nav from "@/components/nav/Nav";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+        <Footer />
+      </body>
       <Toaster richColors />
     </html>
   );
