9bcb30264572f6a573de9d1b545b7e08be122a67
tito-arch <therabbitplc@gmail.com>
2024-02-04 19:37:56 +0300

    Add: microsoft clarity

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kwlbduguj4");
+</script>
+      </head>
       <body className={inter.className}>
         <Nav />
         {children}
