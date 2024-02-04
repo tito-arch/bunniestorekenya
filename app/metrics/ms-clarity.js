e9d6b00618a50fa8ddc298d20198ab7b1b5b1fb1
tito-arch <therabbitplc@gmail.com>
2024-02-04 19:56:30 +0300

    fix: check  if window is defined

--- app/metrics/ms-clarity.js
+++ app/metrics/ms-clarity.js
@@ -1,18 +1,27 @@
 "use client";
 
+import Script from "next/script";
+
 const MicrosoftClarity = () => {
-  return (function (c, l, a, r, i, t, y) {
-    c[a] =
-      c[a] ||
-      function () {
-        (c[a].q = c[a].q || []).push(arguments);
-      };
-    t = l.createElement(r);
-    t.async = 1;
-    t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0];
-    y.parentNode.insertBefore(t, y);
-  })(window, document, "clarity", "script", "kwlbduguj4");
+  // Check if the window object is available (running in the browser)
+  if (typeof window !== "undefined") {
+    // Execute the Clarity script
+    (function (c, l, a, r, i, t, y) {
+      c[a] =
+        c[a] ||
+        function () {
+          (c[a].q = c[a].q || []).push(arguments);
+        };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "kwlbduguj4");
+  }
+
+  // Return null (or any other component structure) since you are not rendering anything
+  return null;
 };
 
 export default MicrosoftClarity;
