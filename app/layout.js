656135a584b8603acdcb9a85adfad43fdbccc491
tito-arch <therabbitplc@gmail.com>
2024-02-04 19:51:22 +0300

    fix: ms clarity tracking implementation

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/components/footer/Footer";
 import Nav from "@/components/nav/Nav";
 import React from "react";
+import MicrosoftClarity from "./metrics/ms-clarity";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,19 +16,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "kwlbduguj4");
-</script>
-      </head>
       <body className={inter.className}>
         <Nav />
         {children}
         <Footer />
+        <MicrosoftClarity />
       </body>
       <Toaster richColors />
     </html>
